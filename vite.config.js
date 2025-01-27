386336b9d514cf22934d95369c2c3cd7c3808f8a
rasheed-abla <rasheedabla08@gmail.com>
2025-01-27 12:33:17 +0100

    base removed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/klarenthal_website/',
+  // base: '/klarenthal_website/',
   plugins: [
     vue(),
   ],
